39587204446a895a4754620fe6428faf61eeebfb
blastz <touchzh1996@outlook.com>
2023-03-10 16:19:50 +0800

    build(nest-grpc-helper): update configs

--- packages/nest-grpc-helper/rollup.config.js
+++ packages/nest-grpc-helper/rollup.config.js
@@ -1,65 +1,57 @@
-import commonjs from "@rollup/plugin-commonjs";
-import { nodeResolve } from "@rollup/plugin-node-resolve";
-import typescript from "@rollup/plugin-typescript";
-import dts from "rollup-plugin-dts";
+import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
 
-const external = [
-  "os",
-  "util",
-  "aliyun-sdk",
-  "winston-daily-rotate-file",
-  "winston-transport",
-  "winston",
-  "triple-beam",
-];
+const external = ['@blastz/logger'];
 
 export default [
   {
-    input: "src/index.ts",
+    input: 'src/index.ts',
     output: [
       {
-        dir: "output/cjs",
-        format: "cjs",
+        dir: 'output/cjs',
+        format: 'cjs',
       },
     ],
     plugins: [
       typescript({
-        outDir: "output/cjs",
+        outDir: 'output/cjs',
         declaration: false,
       }),
       nodeResolve({
         modulesOnly: true,
-        exportConditions: ["node"],
+        exportConditions: ['node'],
       }),
       commonjs(),
     ],
     external,
   },
   {
-    input: "src/index.ts",
+    input: 'src/index.ts',
     output: [
       {
-        dir: "output/esm",
-        format: "esm",
+        dir: 'output/esm',
+        format: 'esm',
       },
     ],
     plugins: [
       typescript({
-        outDir: "output/esm",
+        outDir: 'output/esm',
         declaration: false,
       }),
     ],
-    external: external.concat(["chalk"]),
+    external: external.concat(['chalk']),
   },
   {
-    input: "src/index.ts",
+    input: 'src/index.ts',
     output: [
       {
-        file: "output/index.d.ts",
-        format: "esm",
+        file: 'output/index.d.ts',
+        format: 'esm',
       },
     ],
     plugins: [dts()],
-    external: ["async_hooks"],
+    external: ['async_hooks'],
   },
 ];
