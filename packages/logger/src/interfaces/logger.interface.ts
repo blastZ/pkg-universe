efbeafcae9cd3ba8c47081f45ef1fb2c043ce0d7
blastz <touchzh1996@outlook.com>
2023-03-24 14:47:48 +0800

    fix(logger): type error

--- packages/logger/src/interfaces/logger.interface.ts
+++ packages/logger/src/interfaces/logger.interface.ts
@@ -1,7 +1,11 @@
-import { LeveledLogMethod, Logger as WinstonLogger } from "winston";
+import { LeveledLogMethod, Logger as WinstonLogger } from 'winston';
+import Transport from 'winston-transport';
 
 export interface Logger extends WinstonLogger {
   fatal: LeveledLogMethod;
   trace: LeveledLogMethod;
   child(options: Object): Logger;
+  add(transport: Transport): Logger;
+  remove(transport: Transport): Logger;
+  clear(): Logger;
 }
