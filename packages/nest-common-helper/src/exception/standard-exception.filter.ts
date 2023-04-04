e38a9afe65be4bb5126e264cd359e84c20462639
blastz <touchzh1996@outlook.com>
2023-04-04 14:48:56 +0800

    feat(nest-common-helper): handle error

--- packages/nest-common-helper/src/exception/standard-exception.filter.ts
+++ packages/nest-common-helper/src/exception/standard-exception.filter.ts
@@ -1,4 +1,5 @@
-import { ArgumentsHost, Catch } from '@nestjs/common';
+import { logger } from '@blastz/logger';
+import { ArgumentsHost, Catch, HttpException } from '@nestjs/common';
 import { Response } from 'express';
 import { of } from 'rxjs';
 
@@ -8,11 +9,12 @@ const MESSAGES = {
   UNKNOWN_EXCEPTION_MESSAGE: 'Internal server error',
 };
 
-export interface Params {
+export interface StandardExceptionFilterOptions {
   errorCodePrefix?: string;
+  logError?: boolean;
 }
 
-export interface Payload {
+export interface StandardExceptionFilterPayload {
   data?: any;
   error?: {
     code: string;
@@ -22,16 +24,95 @@ export interface Payload {
 
 @Catch()
 export class StandardExceptionFilter<T = any> {
-  private errorCodePrefix?: string;
+  logger = logger.child({
+    stage: 'nestCommonHelper.standardExceptionFilter',
+  });
 
-  constructor(params: Params = {}) {
-    this.errorCodePrefix = params.errorCodePrefix;
+  constructor(private options: StandardExceptionFilterOptions = {}) {}
+
+  getErrorCode(code: string) {
+    const { errorCodePrefix } = this.options;
+
+    if (errorCodePrefix) {
+      return `${errorCodePrefix}-${code}`;
+    }
+
+    return code;
   }
 
-  public catch(exception: T, host: ArgumentsHost): any {
-    const type = host.getType();
+  private getStandardPayload(exception: StandardException) {
+    const response = exception.getResponse() as StandardExceptionFilterPayload;
+
+    return {
+      data: response.data || {},
+      error: {
+        code: this.getErrorCode(response?.error?.code || '500'),
+        message: response?.error?.message || MESSAGES.UNKNOWN_EXCEPTION_MESSAGE,
+      },
+    };
+  }
+
+  private getHttpPayload(exception: HttpException) {
+    const response: any = exception.getResponse();
+    const code = this.getErrorCode(String(exception.getStatus()));
+
+    const data = {};
+
+    // throw new HttpException('error', 500)
+    if (typeof response === 'string') {
+      return {
+        data,
+        error: {
+          code,
+          message: response,
+        },
+      };
+    }
+
+    // throw new HttpException({ msg: 'error' }, 500);
+    return {
+      data: response.data ?? data,
+      error: {
+        code: response.code ?? code,
+        message: response.message ?? MESSAGES.UNKNOWN_EXCEPTION_MESSAGE,
+      },
+    };
+  }
+
+  private getDefaultPayload() {
+    return {
+      data: {},
+      error: {
+        code: this.getErrorCode('500'),
+        message: MESSAGES.UNKNOWN_EXCEPTION_MESSAGE,
+      },
+    };
+  }
+
+  getPayload(exception: T): StandardExceptionFilterPayload {
+    if (exception instanceof StandardException) {
+      return this.getStandardPayload(exception);
+    }
+
+    if (exception instanceof HttpException) {
+      return this.getHttpPayload(exception);
+    }
+
+    return this.getDefaultPayload();
+  }
+
+  catch(exception: T, host: ArgumentsHost): any {
+    if (this.options.logError) {
+      this.logger.error(exception);
+    }
 
     const payload = this.getPayload(exception);
+
+    return this.handleError(host, payload);
+  }
+
+  handleError(host: ArgumentsHost, payload: StandardExceptionFilterPayload) {
+    const type = host.getType();
 
     if (type === 'rpc') {
       return this.handleRpcError(host, payload);
@@ -44,42 +125,17 @@ export class StandardExceptionFilter<T = any> {
     return this.handleHttpError(host, payload);
   }
 
-  private getErrorCode(code: string) {
-    if (this.errorCodePrefix) {
-      return `${this.errorCodePrefix}-${code}`;
-    }
-
-    return code;
-  }
-
-  private getPayload(exception: T): Payload {
-    if (exception instanceof StandardException) {
-      const response = exception.getResponse() as Payload;
-
-      return {
-        data: response.data || {},
-        error: {
-          code: this.getErrorCode(response?.error?.code || '500'),
-          message:
-            response?.error?.message || MESSAGES.UNKNOWN_EXCEPTION_MESSAGE,
-        },
-      };
-    }
-
-    return {
-      data: {},
-      error: {
-        code: this.getErrorCode('500'),
-        message: MESSAGES.UNKNOWN_EXCEPTION_MESSAGE,
-      },
-    };
-  }
-
-  private handleRpcError(host: ArgumentsHost, payload: Payload) {
+  private handleRpcError(
+    host: ArgumentsHost,
+    payload: StandardExceptionFilterPayload,
+  ) {
     return of(payload);
   }
 
-  private handleWsError(host: ArgumentsHost, payload: Payload) {
+  private handleWsError(
+    host: ArgumentsHost,
+    payload: StandardExceptionFilterPayload,
+  ) {
     const client = host.switchToWs().getClient();
     const callback = host.getArgByIndex(2);
 
@@ -91,7 +147,10 @@ export class StandardExceptionFilter<T = any> {
     }
   }
 
-  private handleHttpError(host: ArgumentsHost, payload: Payload) {
+  private handleHttpError(
+    host: ArgumentsHost,
+    payload: StandardExceptionFilterPayload,
+  ) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
 
