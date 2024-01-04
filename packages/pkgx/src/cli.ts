2756f686da02dc518cd60e290339acd2b96f5bd9
blastz <touchzh1996@outlook.com>
2024-01-04 16:56:06 +0800

    feat(pkgx): build nest next

--- packages/pkgx/src/cli.ts
+++ packages/pkgx/src/cli.ts
@@ -2,6 +2,7 @@
 
 import { program } from 'commander';
 
+import { buildNestNextCommand } from './commands/build-nest-next.cmd.js';
 import { buildCommand } from './commands/build.cmd.js';
 import { publishCommand } from './commands/publish.cmd.js';
 import { serveCommand } from './commands/serve.cmd.js';
@@ -19,6 +20,12 @@ program
     'change default options for application, like disable cjs and dts outputs',
   )
   .action(buildCommand);
+
+program
+  .command('build-nest-next')
+  .description('build next in nest application')
+  .argument('<pkg-relative-path>', 'relative path to pkg root folder')
+  .action(buildNestNextCommand);
 
 program
   .command('serve')
---
+++ packages/pkgx/src/commands/build-nest-next.cmd.ts
@@ -0,0 +1,35 @@
+import { resolve } from 'node:path';
+
+import { $, cd } from 'zx';
+
+import { fillOptionsWithDefaultValue } from '../rollup-utils/fill-options-with-default-value.js';
+import { getPkgxOptions } from '../utils/get-pkgx-options.util.js';
+import { logger } from '../utils/loggin.util.js';
+
+export async function build(pkgRelativePath: string) {
+  logger.cliVersion();
+
+  const pkgPath = resolve(process.cwd(), pkgRelativePath);
+
+  cd(pkgPath);
+
+  const pkgxOptions = await getPkgxOptions();
+
+  const filledPkgxOptions = fillOptionsWithDefaultValue(pkgxOptions);
+
+  const outputDirName = filledPkgxOptions.outputDirName;
+
+  await $`rm -rf src/next/.next`.quiet();
+
+  await $`pnpm next build src/next`;
+
+  await $`rm -rf ${outputDirName}/next`.quiet();
+
+  await $`mkdir -p ${outputDirName}/next`.quiet();
+
+  await $`mv src/next/.next ${outputDirName}/next`.quiet();
+}
+
+export async function buildNestNextCommand(pkgRelativePath: string) {
+  await build(pkgRelativePath);
+}
