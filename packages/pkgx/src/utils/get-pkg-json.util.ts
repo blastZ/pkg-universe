199c76f5e728c852e4305ef51351542c54ebba83
blastz <touchzh1996@outlook.com>
2024-01-25 19:09:54 +0800

    feat(pkgx): empty pkg json

--- packages/pkgx/src/utils/get-pkg-json.util.ts
+++ packages/pkgx/src/utils/get-pkg-json.util.ts
@@ -1,9 +1,37 @@
 import { readFileSync } from 'node:fs';
 
+import chalk from 'chalk';
+
 import { PkgJson } from '../interfaces/pkg-json.interface.js';
 
-export function getPkgJson() {
-  const pkgJson = JSON.parse(readFileSync('./package.json').toString());
+import { logger } from './loggin.util.js';
 
-  return pkgJson as PkgJson;
+let parsedPkgJson: PkgJson | undefined;
+
+export function getPkgJson() {
+  if (parsedPkgJson) {
+    return parsedPkgJson;
+  }
+
+  let pkgJsonStr: string;
+
+  try {
+    pkgJsonStr = readFileSync('./package.json').toString();
+  } catch (err) {
+    logger.warn(
+      chalk.yellow(chalk.bold('package.json not found, using default values.')),
+    );
+
+    pkgJsonStr = '{}';
+  }
+
+  const pkgJson = JSON.parse(pkgJsonStr);
+
+  parsedPkgJson = pkgJson as PkgJson;
+
+  if (typeof parsedPkgJson.name !== 'string') {
+    parsedPkgJson.name = 'anonymous';
+  }
+
+  return parsedPkgJson;
 }
