38402cd9e1d7a0faf9f3bef9f3bf30cf769dceb7
blastz <touchzh1996@outlook.com>
2024-04-29 11:59:54 +0800

    test(zhipuai): chat completions

---
+++ packages/zhipuai/test/chat-completions.spec.ts
@@ -0,0 +1,60 @@
+import { ZhipuAI, type ChatCompletionMessageParams } from '../src/index.js';
+
+const messages: ChatCompletionMessageParams[] = [
+  {
+    role: 'system',
+    content: 'You MUST answer with only the result number.',
+  },
+  {
+    role: 'user',
+    content: '1 + 1 = ?',
+  },
+  {
+    role: 'assistant',
+    content: '2',
+  },
+  {
+    role: 'user',
+    content: '2 + 2 = ?',
+  },
+  {
+    role: 'assistant',
+    content: '4',
+  },
+  {
+    role: 'user',
+    content: '3 + 3 = ?',
+  },
+];
+
+const zhipuai = new ZhipuAI();
+
+describe('zhipuai', () => {
+  it('should work with non streaming', async () => {
+    const result = await zhipuai.chat.completions.create({
+      model: 'glm-3-turbo',
+      messages,
+    });
+
+    // console.log(inspect({ result }, false, 10, true));
+
+    expect(result.choices[0].message.content).toEqual('6');
+  });
+
+  it('should work with streaming', async () => {
+    const completion = await zhipuai.chat.completions.create({
+      model: 'glm-3-turbo',
+      messages,
+      stream: true,
+    });
+
+    let result = '';
+    for await (const chunk of completion) {
+      // console.log(inspect({ chunk }, false, 10, true));
+
+      result += chunk.choices[0].delta.content;
+    }
+
+    expect(result === '6');
+  });
+});
